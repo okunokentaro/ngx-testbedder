888d03e1b3443ca89874bcab85ed8f9041f6452c
OKUNOKENTARO <okunokentaro@me.com>
2017-03-05 13:58:38 +0900

    refactor to class Visitor

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,47 @@
 import * as ts from 'typescript'
 declare const require: any
 
-const files    = ['sample/sample.ts']
-const tsconfig = require('../sample/tsconfig.json')
-const program  = ts.createProgram(files, tsconfig)
+class Visitor {
 
-for (const sourceFile of program.getSourceFiles()) {
-  if (sourceFile.fileName.substr(-5) === '.d.ts') {
-    continue
-  }
-  ts.forEachChild(sourceFile, visit)
-}
+  constructor(files: string[], tsconfig: any) {
+    const program  = ts.createProgram(files, tsconfig)
 
-function visit(node: ts.Node) {
-  if (node.kind === ts.SyntaxKind.ClassDeclaration) {
-    const classDefNode = node as ts.ClassDeclaration
-    if (classDefNode.decorators) {
-      Array.from(classDefNode.decorators).forEach(decoratorNode => {
-        ts.forEachChild(decoratorNode, visitDecorators)
-      })
+    for (const sourceFile of program.getSourceFiles()) {
+      if (sourceFile.fileName.substr(-5) === '.d.ts') {
+        continue
+      }
+      ts.forEachChild(sourceFile, this.visit.bind(this))
     }
   }
-  ts.forEachChild(node, visit)
+
+
+  private visit(node: ts.Node) {
+    if (node.kind === ts.SyntaxKind.ClassDeclaration) {
+      const classDefNode = node as ts.ClassDeclaration
+      if (classDefNode.decorators) {
+        Array.from(classDefNode.decorators).forEach(decoratorNode => {
+          ts.forEachChild(decoratorNode, this.visitDecorators.bind(this))
+        })
+      }
+    }
+    ts.forEachChild(node, this.visit.bind(this))
+  }
+
+  private visitDecorators(node: ts.Node) {
+    if (node.kind === ts.SyntaxKind.CallExpression) {
+      const callNode      = node as ts.CallExpression
+      const decoratorName = (<ts.Identifier>callNode.expression).text
+      console.log('decoratorName', decoratorName)
+    }
+    ts.forEachChild(node, this.visitDecorators.bind(this))
+  }
+
 }
 
-function visitDecorators(node: ts.Node) {
-  if (node.kind === ts.SyntaxKind.CallExpression) {
-    const callNode = node as ts.CallExpression
-    const decoratorName = (<ts.Identifier>callNode.expression).text
-    console.log('decoratorName', decoratorName)
-  }
-  ts.forEachChild(node, visitDecorators)
+const main = () => {
+  const files    = ['sample/sample.ts']
+  const tsconfig = require('../sample/tsconfig.json')
+  new Visitor(files, tsconfig)
 }
+
+main()
