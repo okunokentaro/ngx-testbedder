5cdeedabfe5309f3b7e13c9de6677e98e467a40e
OKUNOKENTARO <okunokentaro@me.com>
2017-03-06 15:41:34 +0900

    refactor

--- src/tree-builder.ts
+++ src/tree-builder.ts
@@ -19,24 +19,33 @@ export class TreeBuilder {
     const buildTree = (solved: Solved, level: number): TreeNode[] => {
       const nextLevel = level + 1
 
+      const nexts = this.solvedPool.filter(node => node.level === nextLevel)
+      if (nextLevel === 1) {
+        const next = nexts[0]
+        const path = next.path
+        const name = next.name
+
+        const childrenNodes = !!next
+          ? buildTree(next, nextLevel)
+          : []
+
+        this.alreadyAddedPaths.add(path)
+
+        return [{
+          path:  path,
+          label: name,
+          nodes: childrenNodes,
+        }]
+      }
+
       return solved.dependencies.toArray().map(classLocation => {
-        const nexts = this.solvedPool.filter(node => node.level === nextLevel)
-
-        const next  = nextLevel === 1
-          ? nexts[0]
-          : nexts.find(v => v.path === classLocation.path)
-
-        const path = nextLevel === 1
-          ? next.path
-          : classLocation.path
+        const next = nexts.find(v => v.path === classLocation.path)
+        const path = classLocation.path
+        const name = classLocation.name
 
         if (this.alreadyAddedPaths.has(path)) {
           return
         }
-
-        const name = nextLevel === 1
-          ? next.name
-          : classLocation.name
 
         const childrenNodes = !!next
           ? buildTree(next, nextLevel)
