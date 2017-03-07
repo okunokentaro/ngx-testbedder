6af365f69b995181a15fe87405c3cd356dc41860
OKUNOKENTARO <okunokentaro@me.com>
2017-03-07 11:38:00 +0900

    improve logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,21 @@
+const chalk = require('chalk')
+
 let verboseLevel = 0
 
 export const setVerboseLevel = (argv: any) => {
   verboseLevel = argv.verbose ? 2 : 0;
 }
 
-export const WARN  = (...args: any[]) => verboseLevel >= 0 && console.log.apply(console, args)
-export const INFO  = (...args: any[]) => verboseLevel >= 1 && console.log.apply(console, args)
-export const DEBUG = (...args: any[]) => verboseLevel >= 2 && console.log.apply(console, [`[verb] ${args[0]}`, ...args.slice(1)])
+export const WARN  = (...args: any[]) => {
+  verboseLevel >= 0 &&
+  console.log.apply(console, args)
+}
+export const INFO  = (...args: any[]) => {
+  verboseLevel >= 1 &&
+  console.log.apply(console, args)
+}
+export const DEBUG = (...args: any[]) => {
+  verboseLevel >= 2 &&
+  console.log.apply(console, [`${chalk.blue('[verb]')} ${args[0]}`, ...args.slice(1)])
+}
 
